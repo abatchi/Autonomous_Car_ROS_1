f3362a747493f43c6c644e83d0305a604c2f4a1a
Nabil Hassan <131000244+NabilHassan11@users.noreply.github.com>
2025-02-22 18:52:33 +0200

    Delete Milestone1/Simulation_Milestone_01_Team_08/Autonomous_System_Project_Team_08/src/Tut1_Ex1.py

--- Milestone1/Simulation_Milestone_01_Team_08/Autonomous_System_Project_Team_08/src/Tut1_Ex1.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import rospy #get the libraries of rospy
-
-rospy.init_node('Tut1_Ex1') #method used to intialize a node
-
-print('Hello World') #to print hello world on the terminal
-
