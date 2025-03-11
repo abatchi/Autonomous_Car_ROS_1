d90851c0c6899077baf11c60a12cb938c96ef697
Nabil <Nabilhesham2017@gmail.com>
2025-03-12 00:47:09 +0200

    Added package files and ensured tracking of src and launch folders

---
+++ Milestone_02_Team_08/autonomous_system_project_team_08/src/Validation_Printing_Node_Team_08.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import rospy #get the libraries of rospy
+
+rospy.init_node('Validation_Printing_Node_Team_08') #method used to intialize a node
+
+for i in range(10):
+    print('Hello World Team_08') #to print hello world on the terminal
+
+
+
+
